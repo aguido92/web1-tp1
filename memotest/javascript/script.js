7aeef7e893b3937ebac6b97deae6199e82b03c98
JuanDeccechis <juandeccechis@gmail.com>
2018-05-13 13:35:11 -0300

    version final, todo revisado, queda en tus manos :)

--- memotest/javascript/script.js
+++ memotest/javascript/script.js
@@ -1,10 +1,12 @@
-'uses strict';
-// Variables
+'use strict';
+// Variables de acceso al HTML
 let btnComenzar = document.getElementById('btn-comenzar');
 let btnReset = document.getElementById('btn-reset');
 let tablero = document.getElementById('tablero');
 let seleccionar = document.getElementById('seleccionar');
 let casillas = document.querySelectorAll('img');
+let tabla = document.getElementById("cuerpoTabla");
+//inicialización de variables locales
 let marcadas = [];
 let aciertos = 0;
 let aciertosTotales = 0;
@@ -14,11 +16,15 @@ let descarte = [];
 let ganadas = 0;
 let tiempo = 3; // 3 segundos por defecto
 let probMarca = 0.50; // 50% por defecto
-////////////////////
-let tabla = document.getElementById("tablaPartidas");
-let filaActual = 1;
+let filaActual = 0;
 
-// Funciones
+
+
+// Funciones internas
+/* muestra el tiempo disponible antes de dar vuelta
+(dejar "boca abajo") las cartas para que inicie el 
+juego. Luego las oculta y prepara la página para que
+el usuario pueda jugar.*/
 function empezarJuego() {
 	crearTablero();
 	visible(tablero);
@@ -41,8 +47,10 @@ function empezarJuego() {
 	},tiempo * 1000);
 }
 
+/* genera el random para saber que cartas están marcadas, 
+cambia la imagen correspondiente (las "da vuelta"), 
+y guarda los índices de las cartas marcadas.*/
 function crearTablero() {
-	alert(probMarca);
     for (let i = 1; i <= casillas.length; i++) {
         if (i === 1 || Math.random() < probMarca) {
             document.getElementById('casillero' + i).src = 'images/marcada.jpg';
@@ -53,48 +61,44 @@ function crearTablero() {
     }
 }
 
+/* deja "boca abajo" las cartas, para que comience a jugar el usuario.*/
 function ocultarMarcas() {
-    //for (let i of marcadas) {
-    //    document.getElementById('casillero' + i).src = 'images/casilla.png';
-    //}
 	for (let img of casillas) {
         img.src = 'images/casilla.jpg';
     }
 }
 
+/* oculta (vuelve invisible) el elemento pasado por parámetro.*/
 function ocultar(elem) {
     elem.classList.add('oculto');
 }
 
+/* deja de ocultar (vuelve visible) el elemento pasado por parámetro.*/
 function visible(elem) {
     elem.classList.remove('oculto');
 }
 
+/* muestra (deja "boca arriba") la carta seleccionada por el usuario
+(solo en caso que esa carta no haya sido previamente seleccionada.*/
 function darVuelta(selected) {
-    if (marcadas.includes(selected)) {
-        if (!descarte.includes(selected)) {
-            document.getElementById('casillero' + selected).src = 'images/marcada.jpg';
+	if (!descarte.includes(selected)){
+		descarte.push(selected);
+		if (marcadas.includes(selected)) {
+			document.getElementById('casillero' + selected).src = 'images/marcada.jpg';
             aciertos ++;
             aciertosTotales ++;
-            descarte.push(selected);
-        } else {
-            // alert('Casillero ' + selected + ' ya seleccionado.');
-        }
-    } else {
-		if (!descarte.includes(selected)) {
+		}
+		else{
 			document.getElementById('casillero' + selected).src = 'images/comun.jpg';
 			errores ++;
 			erroresTotales ++;
-			descarte.push(selected);
-			//setTimeout(function() {
-			//	document.getElementById('casillero' + selected).src = 'images/casilla.png';
-			//},500);
-		} else {
-            // alert('Casillero ' + selected + ' ya seleccionado.');
-        }
-    }
+		}
+	}
 }
 
+/* disparador de funciones, que realiza la lógica de mostrar la carta
+seleccionada por el usuario, actualizar resultados y felicitarlo en 
+caso de haber terminado el juego.*/
 function elegir(selected) {
     darVuelta(selected);
     actualizarResultados();
@@ -103,29 +107,29 @@ function elegir(selected) {
     }
 }
 
-/**************************/
-
-
+/* actualiza la tabla de aciertos y fallas (actuales y totales).*/
 function actualizarResultados() {
-
 	tabla.rows[filaActual].cells[1].innerText = aciertos;
 	tabla.rows[filaActual].cells[2].innerText = errores;
     document.getElementById('cant-aciertos-totales').innerHTML = aciertosTotales;
     document.getElementById('cant-errores-totales').innerHTML = erroresTotales;
 }
 
+/* inserta una nueva fila de aciertos y fallas actuales.*/
 function crearFilaTabla(){
 	filaActual = filaActual+1;
 	let fila = tabla.insertRow(filaActual);
 	let columna0 = fila.insertCell(0);
 	let columna1 = fila.insertCell(1);
 	let columna2 = fila.insertCell(2);
-	columna0.innerHTML = "Partida " + filaActual;
+	let numPartida = filaActual+1;
+	columna0.innerHTML = "Partida " + numPartida;
 	columna1.innerHTML = 0;
 	columna2.innerHTML = 0;
 }
 
-/******************************/
+/* actualiza la cantidad de partidas finalizadas, y felicita al
+usuario cada 3 partidas concecutivas.*/
 function finalizar() {
     ganadas ++;
 	ocultar(seleccionar);
@@ -137,7 +141,8 @@ function finalizar() {
 	},500);
 }
 
-function resetVariables() { // Vuelve a cero los resultados de la partida ganada y los arreglos.
+/* al volver a jugar, reinicia las variables y arreglos que se utilizan.*/
+function resetVariables() { 
     marcadas.splice(0, marcadas.length);
     descarte.splice(0, descarte.length);
     aciertos = 0;
@@ -148,7 +153,8 @@ function resetVariables() { // Vuelve a cero los resultados de la partida ganada
 
 
 // Eventos
-btnComenzar.addEventListener('click', function () { // Funcion que ejecuta al presionar el boton "Comenzar"
+/* Funcion que ejecuta ante el evento: presionar el boton "Comenzar"*/
+btnComenzar.addEventListener('click', function () { 
 	let inputTiempo = document.getElementById('input-tiempo').value;
 	if (inputTiempo.length !== 0) {
 		tiempo = inputTiempo;
@@ -166,6 +172,7 @@ btnComenzar.addEventListener('click', function () { // Funcion que ejecuta al pr
 	}
 });
 
+/* Funcion que ejecuta ante el evento: cambio de selección del usuario*/
 seleccionar.addEventListener('change', function () {
     let selected = seleccionar.selectedIndex;
     if (selected !== 0) {
@@ -173,6 +180,7 @@ seleccionar.addEventListener('change', function () {
     }
 });
 
+/* Funcion que ejecuta ante el evento: presionar el boton "Partida Nueva"*/
 btnReset.addEventListener('click', function(){
 	ocultar(this);
 	ocultar(tablero);
@@ -180,6 +188,4 @@ btnReset.addEventListener('click', function(){
 	visible(botonera);
 	resetVariables();
 	crearFilaTabla();
-	//actualizarResultados();
-	//crear nuevo td(), filaActual++;
 });
